794d0ca9df16ce19a92748fbe5807aaa827926e8
James McLaughlin <e.james.mclaughlin@gmail.com>
2014-03-23 20:10:30 -0400

    First commit on social-swap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SocialSwap::Application.config.secret_key_base = '0aad9f6a3f5af8b719d02a4d1947c5e2e6eb8cf61da56f6e79b10caf4ba60dcaf4f9206fc97b220a3dfa84b44e35b434201b20638ce0f26ae5987dd117fbf279'
